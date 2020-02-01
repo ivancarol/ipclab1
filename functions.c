830c0846c865a9fc794c7e7afd3bb43513dc5dad
ivancarol <icarol@edu.tecnocampus.cat>
2020-02-01 21:35:09 +0100

    afegit step 5 al .h

--- functions.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void test(){
-    printf("Hello world!\n");
-}
--- functions.h
+++ functions.h
@@ -4,6 +4,11 @@
 void step1();
 void step2();
 void step3();
+void step4();
+//void rev_file_line();
+void step5();
+void mostra4();
+long count_characters(FILE *f);
 
 
 #endif // FUNCTIONS_H_INCLUDED
