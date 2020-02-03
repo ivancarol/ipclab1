f1d112d67f9f9b6f0266c65967fe62de7a244799
ivancarol <icarol@edu.tecnocampus.cat>
2020-02-03 21:11:27 +0000

    s'ha corretgit l'ultim missatge printat per pantalla del step4.c

--- step4.c
+++ step4.c
@@ -63,7 +63,7 @@ void step4(){
         }
         fputc('\n', dfile); //Adds the remaining \n
     }
-    printf("\nContents copied to %s with the number of chars per line.\n", filename);
+    printf("\nContents copied to %s.\n", filename);
 
     fclose(sfile);
     fclose(dfile);
